f5da960b36e54fe5c4aec443391d49adcebac7ca
Atul Srivastava <Atul.kumarSrivastava@iddinkgroup.com>
2018-10-13 12:08:28 +0530

    spell corrections

--- DSImplementations/src/com/atulmaps/adt/Main.java
+++ DSImplementations/src/com/atulmaps/adt/Main.java
@@ -3,7 +3,7 @@ package com.atulmaps.adt;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to DS and Algoriths world.");
+		System.out.println("Welcome to DS and Algorithms world.");
 
 	}
 
