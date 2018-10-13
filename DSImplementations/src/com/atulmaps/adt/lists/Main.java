c659d9890e0693b4fc47e491c0f66b4916a77b89
Atul Srivastava <Atul.kumarSrivastava@iddinkgroup.com>
2018-10-13 13:30:40 +0530

    singly linked list implementation with insertAtHead operation

--- DSImplementations/src/com/atulmaps/adt/lists/Main.java
+++ DSImplementations/src/com/atulmaps/adt/lists/Main.java
@@ -3,7 +3,16 @@ package com.atulmaps.adt.lists;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Lists world.");
+
+		SinglyLinkedList list = new SinglyLinkedList();
+
+		list.insertAtHead(2);
+		list.insertAtHead(7);
+		list.insertAtHead(1);
+		list.insertAtHead(20);
+		list.insertAtHead(9);
+
+		System.out.println(list.toString());
 
 	}
 
